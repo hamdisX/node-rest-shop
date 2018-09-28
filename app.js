c198b67e770964b1800d4d901647d46aad120509
amdi <hamdismiled@gmail.com>
2018-09-28 07:29:54 -0700

    add logger midlware (morgan)  and ErrorHandler

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express =require("express")
+const morgan= require("morgan")
 const app = express();
 const {router,hmd} = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-console.log("test export", hmd)
+//console.log("test export", hmd)
+
+// morgan : logger screen (dev,combined)
+app.use(morgan('dev'));
+
+
+// Routes which should handle requests
 app.use('/products', router);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    console.log('error msg',error.message)
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 
 module.exports= app ;
