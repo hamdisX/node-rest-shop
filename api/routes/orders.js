cbd9ba7938a647e1ecbe237eb526be2a994ee8dd
Hamdi MILED <hamdi.miled@sofia.local>
2018-10-01 16:09:33 +0100

    add Orders method  Please enter the commit message for your changes. Lines starting

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,17 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require("mongoose")
+const Product = require("../models/product")
+const Order = require("../models/orders")
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders were fetched'
-    });
+    Order.find().populate("product").select("product.name").then(
+        orders=>{
+            res.status(200).json(
+                {
+                    orders
+                }
+            )
+        }
+    )
 });
 
-router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message: 'Order was created'
-    });
-});
+
+
+router.post("/", (req, res, next) => {
+    Product.findById(req.body.productId)
+      .then(product => {
+        if (!product) {
+          return res.status(404).json({
+            message: "Product not found"
+          });
+        }
+        const order = new Order({
+          _id: mongoose.Types.ObjectId(),
+          quantity: req.body.quantity,
+          product: req.body.productId
+        });
+        return order.save();
+      }).then(
+          rslt=>{
+              res.status(202).json({
+                
+                result:rslt
+              })
+          }
+      )
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
+
+
+
+
+
 
 router.get('/:orderId', (req, res, next) => {
     res.status(200).json({
--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const orderRoutes = require('./api/routes/orders');
 
 mongoose.connect('mongodb+srv://amdi:'+process.env.MONGO_ATLAS_PW +'@node-rest-api-qiavb.mongodb.net/test?retryWrites=true'
 ,{ useCreateIndex: true,
-    useNewUrlParser: true })
+    useNewUrlParser: true,
+    autoIndex: false })
 mongoose.set('useCreateIndex', true);
 
  app.use(bodyParser.urlencoded({ extended: false }));
